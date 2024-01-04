691a6ffd4004ede9481224ce1a1d0ab600b78d21
gsaujla <gurmeharsingh7711@gmail.com>
2024-01-04 00:20:02 -0500

    Created Second Version of Finance Track

--- tracker/models.py
+++ tracker/models.py
@@ -1,3 +1,26 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Profile(models.Model):
+    user = models.ForeignKey(User , on_delete = models.CASCADE)
+    income = models.IntegerField()
+    balance = models.FloatField(null = True)
+    expenditure = models.FloatField()
+
+
+
+
+class activity(models.Model):
+    user = models.ForeignKey(User , on_delete = models.CASCADE)
+    description = models.CharField(max_length = 150)
+    amount = models.FloatField()
+    
+    gain = models.BooleanField() 
+
+    def __str__(self):
+        return self.description
+    
+
+ 
+  
--- tracker/views.py
+++ tracker/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
-
+from django.shortcuts import render ,redirect
+from .models import *
+from django.http import HttpResponseRedirect
 # Create your views here.
+
+def index(request):
+    profile = Profile.objects.filter( user = request.user ).first()
+    if request.method == 'POST':
+        obtained_text = request.POST.get("descp")
+        obtained_amount = request.POST.get("amount")
+        nature = request.POST.get("Bool")
+        
+        new_activity = activity(user = request.user ,description = obtained_text , amount = obtained_amount , gain = nature)
+        new_activity.save()
+        print(nature)
+
+        if nature is True:
+            profile.balance += float(obtained_amount)
+            
+        else:
+            profile.balance -= float(obtained_amount)
+            profile.expenditure  += float(obtained_amount)
+            
+        profile.save()
+        return redirect('/')
+             
+
+
+
+    
+    return render(request,'tracker/index.html',{'profile' : profile})
+
